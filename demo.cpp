5c6249c3b5be53cde76d4874de82861b200f463c
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-11-13 11:01:48 +0530

    pull

--- demo.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>  
-
-int main() {
-    int a = 9;
-    int b = 9;
-    std::cout << a - b << std::endl;   
-    return 0; 
-}
--- undersentence.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-using namespace std ;
-int main (){
-    cout <<"hi i am stark" << endl << "hi i am tony" << endl;
-    cout <<"hi i am tony stark";
-    return 0;
-}
