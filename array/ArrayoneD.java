89243407bb8598aab7d4a8107a3604149452975a
Chinmya <105976402+chinmayabeheragit@users.noreply.github.com>
2024-12-13 00:06:51 +0530

    push array one d basic 1

---
+++ array/ArrayoneD.java
@@ -0,0 +1,8 @@
+public class ArrayoneD {
+    public static void main(String[] args) {
+        int [] arr = {10,20,30,40,50,60,70,80,90,100};
+        for (int i=0; i<arr.length; i++){
+            System.out.println(arr[i]+"");
+        }
+    }
+}
